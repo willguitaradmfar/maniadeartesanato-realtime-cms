76a8aaf42e9414fbd1be27325e2f6d3a57c14e00
William Lima Pereira <william.pereira@totvs.com.br>
2013-08-26 17:41:28 -0300

    ajuste de css
    Signed-off-by: William Lima Pereira <william.pereira@totvs.com.br>

---
+++ public/javascripts/vendors/gallery/template.js
@@ -0,0 +1,15 @@
+			<div class="rg-image-wrapper">
+				{{if itemsCount > 1}}
+					<div class="rg-image-nav">
+						<a href="#" class="rg-image-nav-prev">Previous Image</a>
+						<a href="#" class="rg-image-nav-next">Next Image</a>
+					</div>
+				{{/if}}
+				<div class="rg-image"></div>
+				<div class="rg-loading"></div>
+				<div class="rg-caption-wrapper">
+					<div class="rg-caption" style="display:none;">
+						<p></p>
+					</div>
+				</div>
+			</div>
