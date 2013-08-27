049b4a66aa1b35291172ce9c1c329276b218cf7f
Arabeths <arabeths@Arabeths.(none)>
2013-08-27 18:32:18 +0000

    mania
    Signed-off-by: Arabeths <arabeths@Arabeths.(none)>

---
+++ startProd.sh
@@ -0,0 +1,1 @@
+export TMP=/var/www/maniadeartesanato/tmp;export PATH_UPLOAD=/var/www/maniadeartesanato/upload;export host=http://maniadeartesanato.com.br;export consumerKey=6NFX5SIU3IiZYxcLfyGw5A;export consumerSecret=Nx5VwraYF0b1pAOZa7Cy3RXN84Mk7jHBzCzo41GNg;npm start
