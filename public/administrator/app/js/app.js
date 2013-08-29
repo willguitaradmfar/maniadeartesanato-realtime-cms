a49b7915821d14bee3156b987578cdc92d876f15
William Lima Pereira <william.pereira@totvs.com.br>
2013-08-29 17:53:43 -0300

    comitar
    Signed-off-by: William Lima Pereira <william.pereira@totvs.com.br>

--- public/administrator/app/js/app.js
+++ public/administrator/app/js/app.js
@@ -6,6 +6,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/articles', {templateUrl: 'partials/articles.html', controller: 'ArticlesControle'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/fotos', {templateUrl: 'partials/fotos.html', controller: 'FotosControle'});
     $routeProvider.otherwise({redirectTo: '/articles'});
  }]);
 
--- public/administrator/app/js/controllers.js
+++ public/administrator/app/js/controllers.js
@@ -109,23 +109,129 @@ angular.module('myApp.controllers', []).
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
   	console.log('MyCtrl2');	
-  }])
-  
+  }])  
   .controller('MenuControle', ['$scope', function($scope) {
-  	console.log('MenuControle');
- 	
+  	console.log('MenuControle');	
 
-	$scope.atMenu = function($t) {		
-		if($t == 'post'){
-			$scope.classMenuPost = 'active';
-			$scope.classMenuUser = '';
-			$scope.title = 'Postagens';
-		}else if($t == 'users'){
-			$scope.classMenuUser = 'active';
-			$scope.classMenuPost = '';
-			$scope.title = 'Usuários';
-		}
-	};
+  	$scope.atMenu = function($t) {		
+      $scope.classMenuUser = '';
+      $scope.classMenuPost = '';
+      $scope.classMenuFotos = '';
+  		if($t == 'post'){
+  			$scope.classMenuPost = 'active';			
+  			$scope.title = 'Postagens';
+  		}else if($t == 'users'){
+  			$scope.classMenuUser = 'active';			
+  			$scope.title = 'Usuários';
+  		}else if($t == 'fotos'){
+        $scope.classMenuFotos = 'active';      
+        $scope.title = 'Fotos';
+      }
+  	};
+
+  }])
+
+  .controller('FotosControle', ['$scope', '$http', '$templateCache', function($scope, $http, $templateCache) {
+    console.log('FotosControle');
+
+    
+    $scope.title = 'Fotos';
+
+    $scope.msg = {};    
+
+    $scope.refresh = function(foto) {
+      $http({method: 'GET', url: '/fotos/all', cache: $templateCache}).
+            success(function(data, status, headers, config) {
+                $scope.fotos = data;                
+            }).
+            error(function(data, status, headers, config) {
+                
+            }); 
+    };
+
+    
+
+     $scope.upload = function() {      
+      //$('#uploadFoto').submit();
+
+      $http({method: 'POST', url: '/file-upload', data: $scope.newFoto.nameFile}) 
+    };
+
+    $scope.editar = function(foto) {
+      console.log(foto._id);
+      $scope.newFoto = foto;    
+      
+    };
+
+    $scope.novo = function() {      
+      $scope.newFoto = {};
+      
+    };
+
+    $scope.salvar = function(foto) {
+    if(foto && foto._id){
+        $scope.newFoto = foto;      
+        $scope.newFoto.content = $scope.jval.html();
+
+        $http({method: 'PUT', url: '/foto/'+foto._id, data : $scope.newFoto, cache: $templateCache}).
+              success(function(data, status, headers, config) {             
+                  $scope.msg.type = 'alert alert-success';
+                  $scope.msg.title = 'Sucesso';
+                  $scope.msg.content = 'Foto ('+$scope.newFoto.title+') Atualizado com sucesso';                                
+              }).
+              error(function(data, status, headers, config) {
+                  $scope.msg.type = 'alert alert-error';
+                  $scope.msg.title = 'Erro';
+                  $scope.msg.content = 'Foto '+$scope.newFoto.title+' com erro ('+data+')';
+              });
+      }else{        
+        $scope.newFoto.content = $scope.jval.html();                
+        $scope.newFoto.slug = 'teste';
+        $scope.newFoto.state = 'Publish';        
+        $scope.newFoto.views = 0;
+        $scope.newFoto.comments = [];
+
+
+        $http({method: 'POST', url: '/foto/', data : $scope.newFoto, cache: $templateCache}).
+              success(function(data, status, headers, config) { 
+                  $scope.newFoto._id = data.id;
+                  $scope.msg.type = 'alert alert-success';
+                  $scope.msg.title = 'Sucesso';
+                  $scope.msg.content = 'Foto ('+$scope.newFoto.title+') Salvo com sucesso';                                
+                  $scope.fotos.push($scope.newFoto);                   
+              }).
+              error(function(data, status, headers, config) {
+                  $scope.msg.type = 'alert alert-error';
+                  $scope.msg.title = 'Erro';
+                  $scope.msg.content = 'Foto '+$scope.newFoto.title+' com erro ('+data+')';
+              });
+      }
+    };
+
+    $scope.excluir = function(foto) {
+      console.log(foto._id);
+      $scope.newFoto = foto;      
+      
+      $http({method: 'DELETE', url: '/foto/'+foto._id, data : $scope.newFoto, cache: $templateCache}).
+            success(function(data, status, headers, config) {             
+                $scope.msg.type = 'alert alert-success';
+                $scope.msg.title = 'Sucesso';
+                $scope.msg.content = 'Foto ('+$scope.newFoto.title+') excluído com sucesso';
+                
+                foto.isExcluir = 'none';
+                foto.isEditar = 'none';
+                foto.classNameRow = 'error';
+
+
+            }).
+            error(function(data, status, headers, config) {
+                $scope.msg.type = 'alert alert-error';
+                $scope.msg.title = 'Erro';
+                $scope.msg.content = 'Foto '+$scope.newFoto.title+' com erro ('+data+')';
+            });
+    };
+
+    $scope.refresh();
 
   }]);
 
