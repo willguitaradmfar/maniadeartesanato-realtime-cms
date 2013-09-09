25d3f2b97151077c1738c4a3453984294078aacd
William Lima Pereira <william.pereira@totvs.com.br>
2013-09-09 09:43:08 -0300

    commit test
    Signed-off-by: William Lima Pereira <william.pereira@totvs.com.br>

---
+++ public/javascripts/gallery/gallery.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#makeMeScrollable").smoothDivScroll({
+        mousewheelScrolling: "allDirections",
+        manualContinuousScrolling: true,
+        autoScrollingMode: "onStart"
+    });
+    alert('teste');
+});
